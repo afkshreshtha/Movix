0d7d7123f0d08f7026d4b76d97e6b9b6385ce749
afkshreshtha <125040500+afkshreshtha@users.noreply.github.com>
2023-05-17 10:18:12 +0530

    Update Popular.jsx

--- src/pages/home/popular/Popular.jsx
+++ src/pages/home/popular/Popular.jsx
@@ -9,7 +9,7 @@ import useFetch from '../../../hooks/useFetch'
 const Popular = () => {
   const [endpoint, setEndpoint] = useState('movie')
 
-  const { data, loading } = useFetch(`/${endpoint}/popular?language=hi-IN`)
+  const { data, loading } = useFetch(`/${endpoint}/popular`)
 
   const onTabChange = (tab) => {
     setEndpoint(tab === 'Movies' ? 'movie' : 'tv')
